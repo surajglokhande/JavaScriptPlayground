7f5e47e664461c0203562ebf3a5de16ecac80eba
surajglokhande <surajglokhande@gmail.com>
2025-08-01 13:56:35 +0530

    Add arrow function examples with commented console logs

---
+++ arrow-functions.js
@@ -0,0 +1,17 @@
+const add = () => {
+    return 5 + 5
+}
+//console.log(add())
+const sub = () => 10 - 5
+//console.log(sub())
+
+const addition = (num1, num2) => {
+    return num1 + num2
+}
+//console.log(addition(10, 10))
+const substarction = num1 => {
+    return num1 - 5
+}
+// console.log(substarction(15))
+const abc = count => count + 1;
+// console.log(abc(1))
