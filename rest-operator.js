5ad30da3716711a0c1104121ab0ab0a5c9ed04a6
surajglokhande <surajglokhande@gmail.com>
2025-09-09 11:45:40 +0530

    Add rest and spread operator examples in separate files for better organization

--- rest-operator.js
+++ rest-operator.js
@@ -22,22 +22,3 @@ const obj = student
 // console.log(obj.name, obj.age, other);
 // console.log(name, age, { ...other })
 
-//spread operator
-
-function spreadDemo(num1, num2, num3, num4, num5) {
-    console.log(num1, num2, num3, num4, num5);
-}
-
-const array = [10, 20, 30, 40, 50];
-//spreadDemo(array[0],array[1],array[2]);
-//spreadDemo(...array);
-//console.log(newArray = [...array, 60])
-
-//spread with object to override
-
-const newStudent = {
-    ...student,
-    compnay: "Infosys"//override compnay 
-}
-
-//console.log(newStudent);
---
+++ spread-operator.js
@@ -0,0 +1,40 @@
+//spread operator
+
+function spreadDemo(num1, num2, num3, num4, num5) {
+    console.log(num1, num2, num3, num4, num5);
+}
+
+const array = [10, 20, 30, 40, 50];
+//spreadDemo(array[0],array[1],array[2]);
+//spreadDemo(...array);
+//console.log(newArray = [...array, 60])
+
+//spread with object to override
+
+const student = {
+    name: "suraj",
+    age: 34,
+    compnay: "TCS",
+    address: {
+        country: "India"
+    }
+}
+
+const newStudent = {
+    ...student,
+    compnay: "Infosys"//override compnay 
+}
+
+// console.log(newStudent);
+
+const newS = {
+    ...student,
+    address: {
+        country: "US"
+    }
+}
+// console.log(newS);
+
+const newStudentFunc = { ...newS, address: { ...newS.address, country: "UK" } }
+
+console.log(newStudentFunc)
