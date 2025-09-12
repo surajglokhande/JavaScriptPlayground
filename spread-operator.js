195a5b49f2ebdcdb14bc62f2e26531146513a1f7
surajglokhande <surajglokhande@gmail.com>
2025-09-12 10:15:50 +0530

    Comment out console.log for newArray in spread operator example

--- spread-operator.js
+++ spread-operator.js
@@ -7,7 +7,7 @@ function spreadDemo(num1, num2, num3, num4, num5) {
 const array = [10, 20, 30, 40, 50];
 //spreadDemo(array[0],array[1],array[2]);
 //spreadDemo(...array);
-console.log(newArray = [...array, 60])
+//console.log(newArray = [...array, 60])
 
 //spread with object to override
 
